cb55714091e0b201239980af56fd6ad63188c134
Maksim Sharnin <maksim.sharnin@gmail.com>
2018-02-01 15:17:56 +0300

    async spawn

--- src/plugins/sms.js
+++ src/plugins/sms.js
@@ -1,9 +1,9 @@
-const { spawn } = require('child_process');
+const spawn = require('await-spawn');
 const config = require("../lib/config");
 
 async function run({ phone, text }) {
   const cmd = `echo '${text}' | gammu sendsms TEXT '${phone}' -unicode`
-  spawn('ssh', ['-t', config.gammuHost, cmd]);  
+  await spawn('ssh', ['-t', config.gammuHost, cmd]);  
 }
 
 module.exports = run;
